5e439f83bb65cc8110115fba2a4295163c61026c
Mario Hernandez <mhmario963@gmail.com>
2025-08-20 23:03:50 -0400

    add new local development script.

---
+++ runner.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+BINARY_NAME="advance"
+
+if [ -f "$BINARY_NAME" ]; then
+	echo "removing old binary..."
+	rm "$BINARY_NAME"
+fi
+
+
+echo "building new binary..."
+
+go build -o "$BINARY_NAME" .
+
+if [ $? -eq 0 ]; then
+	echo "running.."
+	echo
+
+	./"$BINARY_NAME"
+	EXIT_CODE=$?
+	exit $EXIT_CODE
+else
+	echo "Build failed. Not running." >&2
+	exit 1
+fi
+
