b826fdd268c70518265b93a8bcdb975cde26327f
Mario Hernandez <mhmario963@gmail.com>
2025-09-07 21:12:18 -0400

    implement philosopher dinner simualation.

--- main.go
+++ main.go
@@ -1,8 +1,26 @@
 package main
 
-import "advance-go-concurrency-patterns/philosopher"
+import (
+	"advance-go-concurrency-patterns/philosopher"
+	"context"
+	"fmt"
+	"sync"
+	"time"
+)
 
 func main() {
 	dinner := philosopher.NewDinner(5)
-	dinner.Start()
+
+	ctx, cancel := context.WithCancel(context.Background())
+	var wg sync.WaitGroup
+
+	dinner.Start(ctx, &wg)
+
+	time.Sleep(5 * time.Second)
+	fmt.Println("cancelling the simulation")
+	cancel()
+
+	wg.Wait()
+
+	fmt.Println("see ya!")
 }
--- philosopher/dining.go
+++ philosopher/dining.go
@@ -1,24 +1,69 @@
 package philosopher
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+	"sync"
+	"time"
+)
 
 type Dinner struct {
-	numPhilosophers int
-	numForks        int
+	pCount int
+	forks  []sync.Mutex
 }
 
 func NewDinner(numPhilosophers int) *Dinner {
-	// There are always the same number of philospoher as there are forks.
 	return &Dinner{
-		numPhilosophers: numPhilosophers,
-		numForks:        numPhilosophers,
+		pCount: numPhilosophers,
+		forks:  make([]sync.Mutex, numPhilosophers, numPhilosophers),
 	}
 }
 
-func (d *Dinner) Start() {
-	fmt.Printf(
-		"starting dinner with %v philosopohers and %v forks.\n",
-		d.numPhilosophers,
-		d.numForks,
-	)
+func (d *Dinner) Start(ctx context.Context, wg *sync.WaitGroup) {
+	for i := 0; i < d.pCount; i++ {
+		wg.Add(1)
+		go d.philosopherWorker(ctx, i, wg)
+	}
+}
+
+func (d *Dinner) philosopherWorker(ctx context.Context, pID int, wg *sync.WaitGroup) {
+	defer wg.Done()
+
+	for {
+		select {
+		case <-ctx.Done():
+			fmt.Printf("philosopher=%v worker shutting down\n", pID)
+			return
+		default:
+			d.think(pID)
+			leftFork := &d.forks[pID]
+			rightFork := &d.forks[(pID-1+d.pCount)%d.pCount]
+
+			if pID == d.pCount-1 {
+				// prevent the case where all the philosophers grab their left fork successfully
+				// and now the system is in dead-lock.
+				d.eat(pID, rightFork, leftFork)
+			} else {
+				d.eat(pID, leftFork, rightFork)
+			}
+		}
+	}
+}
+
+func (d *Dinner) think(pID int) {
+	fmt.Printf("philosopher=%v thinking.\n", pID)
+	time.Sleep(2 * time.Second)
+	fmt.Printf("philosopher=%v done thinking.\n", pID)
+}
+
+func (d *Dinner) eat(pID int, lf, rf *sync.Mutex) {
+	lf.Lock()
+	defer lf.Unlock()
+
+	rf.Lock()
+	defer rf.Unlock()
+
+	fmt.Printf("philosopher=%v eating.\n", pID)
+	time.Sleep(1 * time.Second)
+	fmt.Printf("philosopher=%v done eating.\n", pID)
 }
