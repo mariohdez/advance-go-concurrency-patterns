e97d3f7dfcb8a55ce47968b743bdf7677d9fee75
Mario Hernandez <mhmario963@gmail.com>
2025-11-15 17:26:57 -0500

    worker pool.

--- main.go
+++ main.go
@@ -1,70 +1,105 @@
 package main
 
 import (
-	"fmt"
-	"math/rand/v2"
+	"bufio"
+	"io/fs"
+	"log"
+	"log/slog"
+	"os"
+	"path/filepath"
+	"regexp"
 	"sync"
 	"time"
 )
 
+type Work struct {
+	file  string
+	regex *regexp.Regexp
+}
+
 func main() {
-	inputCh := make(chan int)
-	doneCh := make(chan struct{})
+	start := time.Now()
+	defer func() {
+		dur := time.Since(start)
+		slog.Info("Operation finished",
+			slog.Duration("execution_time", dur),
+		)
+	}()
+	if len(os.Args) < 3 {
+		log.Fatal("Usage: go run main.go <directory> <regex>")
+	}
 
-	var pWG sync.WaitGroup
-	for i := range 10 {
-		pWG.Add(1)
+	workCh := make(chan Work, 100)
+
+	var wg sync.WaitGroup
+	for range 100 {
+		wg.Add(1)
+
 		go func() {
-			defer pWG.Done()
-			producer(i, inputCh, doneCh)
+			defer wg.Done()
+
+			Worker(workCh)
 		}()
 	}
 
-	var cWG sync.WaitGroup
-	for i := range 10 {
-		cWG.Add(1)
-		go func() {
-			defer cWG.Done()
-			consumer(i, inputCh)
-		}()
+	re, err := regexp.Compile(os.Args[2])
+	if err != nil {
+		log.Fatalf("compile regex %s", os.Args[2])
 	}
 
-	time.Sleep(3 * time.Second)
-	close(doneCh)
+	var senderWg sync.WaitGroup
 
-	pWG.Wait()
-	close(inputCh)
+	senderWg.Add(1)
+	go func() {
+		defer senderWg.Done()
 
-	cWG.Wait()
+		filepath.Walk(os.Args[1], func(path string, info fs.FileInfo, err error) error {
+			if err != nil {
+				slog.Warn("unable to walk path", "path", path, "error", err)
+				return nil
+			}
 
-	fmt.Println("good bye!")
+			if info.IsDir() {
+				return nil
+			}
+
+			workCh <- Work{
+				file:  path,
+				regex: re,
+			}
+			return nil
+		})
+	}()
+
+	senderWg.Wait()
+	close(workCh)
+	wg.Wait()
 }
 
-func producer(index int, inputCh chan<- int, doneCh <-chan struct{}) {
-	for {
-		select {
-		case <-doneCh:
-			return
-		default:
-			// no-op
-		}
-
-		v := rand.IntN(10)
-
-		time.Sleep(time.Millisecond * time.Duration(rand.IntN(1000)))
-
-		select {
-		case inputCh <- v:
-		case <-doneCh:
-			return
-		}
-
-		fmt.Printf("Producer[%d]: sent=%d\n", index, v)
+func Worker(workCh chan Work) {
+	for work := range workCh {
+		processWork(work.file, work.regex)
 	}
 }
 
-func consumer(index int, inputCh <-chan int) {
-	for v := range inputCh {
-		fmt.Printf("Consumer[%d]: recieved=%d\n", index, v)
+func processWork(fn string, re *regexp.Regexp) {
+	f, err := os.Open(fn)
+	if err != nil {
+		slog.Error("unable to open file", "file", fn, "error", err)
+		return
+	}
+	defer func() {
+		_ = f.Close()
+	}()
+
+	scn := bufio.NewScanner(f)
+	lineNum := 1
+	for scn.Scan() {
+		result := re.Find(scn.Bytes())
+		if len(result) > 0 {
+			slog.Info("Match found", "file", fn, "line", lineNum, "match", string(result))
+		}
+
+		lineNum++
 	}
 }
